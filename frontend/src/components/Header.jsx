42020bcabe9ab3536570bae5facae9bfc147eb15
Galen Casstevens <galencasstevens@gmail.com>
2022-06-29 17:06:58 -0400

    started add frontend pages and components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+function Header() {
+	return (
+		<header className="header">
+			<div className="logo">
+				<Link to="/register">
+					<FaUser /> Register
+				</Link>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
