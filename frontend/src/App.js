6b4d90c475550940599ebae812543b4f5ce6a259
Galen Casstevens <galencasstevens@gmail.com>
2022-06-29 23:19:13 -0400

    added a header component and started building out the register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -8,6 +10,7 @@ function App() {
 		<>
 			<Router>
 				<div className="container">
+					<Header />
 					<Routes>
 						<Route path="/" element={<Home />}></Route>
 						<Route path="/login" element={<Login />}></Route>
@@ -15,6 +18,7 @@ function App() {
 					</Routes>
 				</div>
 			</Router>
+			<ToastContainer />
 		</>
 	);
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,10 +5,20 @@ function Header() {
 	return (
 		<header className="header">
 			<div className="logo">
-				<Link to="/register">
-					<FaUser /> Register
-				</Link>
+				<Link to="/">Support Desk</Link>
 			</div>
+			<ul>
+				<li>
+					<Link to="/login">
+						<FaSignInAlt /> Login
+					</Link>
+				</li>
+				<li>
+					<Link to="/register">
+						<FaUser /> Register
+					</Link>
+				</li>
+			</ul>
 		</header>
 	);
 }
