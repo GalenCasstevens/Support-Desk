2c6084c06ef898c907cdb5a769a4acd3bede3b28
Galen Casstevens <galencasstevens@gmail.com>
2022-07-25 23:55:15 -0400

    started adding a route guard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NewTicket from './pages/NewTicket';
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 						<Route path="/" element={<Home />}></Route>
 						<Route path="/login" element={<Login />}></Route>
 						<Route path="/register" element={<Register />}></Route>
+						<Route path="/new-ticket" element={<NewTicket />}></Route>
 					</Routes>
 				</div>
 			</Router>
