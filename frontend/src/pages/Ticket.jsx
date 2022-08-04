9894c91771774e0321c9db2e0318a9e4b1eec755
Galen Casstevens <galencasstevens@gmail.com>
2022-08-04 11:56:54 -0400

    started building out ticket view

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -16,12 +16,49 @@ function Ticket() {
 	const { ticketId } = useParams();
 
 	useEffect(() => {
+		return () => {
+			if (isSuccess) {
+				dispatch(reset());
+			}
+		};
+	}, [dispatch, isSuccess]);
+
+	useEffect(() => {
 		if (isError) toast.error(message);
 
 		dispatch(getTicket(ticketId));
+		// eslint-disable-next-line
 	}, [isError, message, ticketId]);
 
-	return <div>Ticket</div>;
+	if (isLoading) {
+		return <Spinner />;
+	}
+
+	if (isError) {
+		return <h3>Something Went Wrong</h3>;
+	}
+
+	return (
+		<div className="ticket-page">
+			<header className="ticket-header">
+				<BackButton url="/tickets" />
+				<h2>
+					Ticket ID: {ticket._id}
+					<span className={`status status-${ticket.status}`}>
+						{ticket.status}
+					</span>
+				</h2>
+				<h3>
+					Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
+				</h3>
+				<hr />
+				<div className="ticket-desc">
+					<h3>Description of Issue</h3>
+					<p>{ticket.description}</p>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default Ticket;
